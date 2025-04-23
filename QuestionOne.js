9879993ddb9fa5a2cb356a655137a5d1893b310e
Wiseman <25WisemanN@badinhs.org>
2025-04-23 13:15:32 -0400

    guess i forgot to add Question one

--- QuestionOne.js
+++ QuestionOne.js
@@ -1,15 +1,15 @@
 function Primes(n) {
-    let factors = [0];
-    let start = 2;
+    let factors = [0]
+    let start = 2
   
     while (n >= 2) {
       if (n % start === 0) {
-        factors.push(start);
-        n = n / start;
+        factors.push(start)
+        n = n / start
       } else {
-        start++;
+        start++
       }
     }
-    return factors[factors.length-1];
+    return factors[factors.length-1]
   }
   console.log(Primes(600851475143))
